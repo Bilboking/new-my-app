9dc77d9754f6ec086ca25c5a9e1cb823af8960e8
Bilboking <brkbendreality@gmail.com>
2021-02-27 15:24:29 -0600

    new commit

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,18 @@ const Home = () => {
     const handleClick = (e) => {
         console.log('hello people', e)
     }
-//** creating an event that can pass in a string etc.   */
+//** creating an event that can pass in a string, function argument etc.   */
 //** goes with line 16 button 'handleClickAgain' */
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name)
+    const handleClickAgain = (name,e) => {
+        console.log('hello ' + name, e.target)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={ handleClick }>Click me</button>
-            <button onClick={() => 
-                handleClickAgain('mario')
+            <button onClick={(e) => 
+                handleClickAgain('mario', e)
             }>Click me again</button>
         </div>
  );
